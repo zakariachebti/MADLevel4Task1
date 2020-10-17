e2bc15e141e7156289974366157534dc4c4712ce
Zakaria Chebti <zakaria@chipta.com>
2020-10-17 23:38:09 +0200

    Step 2

---
+++ app/src/main/java/com/example/madlevel4task1/ProductRepository.kt
@@ -0,0 +1,30 @@
+package com.example.madlevel4task1
+
+import android.content.Context
+
+class ProductRepository(context: Context) {
+
+    private val productDao: ProductDao
+
+    init {
+        val database = ShoppingListRoomDatabase.getDatabase(context)
+        productDao = database!!.productDao()
+    }
+
+    suspend fun getAllProducts(): List<Product> {
+        return productDao.getAllProducts()
+    }
+
+    suspend fun insertProduct(product: Product) {
+        productDao.insertProduct(product)
+    }
+
+    suspend fun deleteProduct(product: Product) {
+        productDao.deleteProduct(product)
+    }
+
+    suspend fun deleteAllProducts() {
+        productDao.deleteAllProducts()
+    }
+
+}
